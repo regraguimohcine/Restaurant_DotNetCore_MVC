fab73d8a901de828c0f5b1165afb91f9cfaf238e
unknown <mohcineregragui5@gmail.com>
2022-02-12 22:55:48 +0100

    First commit

---
+++ Restaurant_DotnetCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Restaurant_DotnetCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
